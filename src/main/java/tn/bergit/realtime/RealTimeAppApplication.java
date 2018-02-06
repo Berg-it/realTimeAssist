179a3826608ebdd83b4bf426c3ca6575a02a1063
BERGUIGA AMINE <amine.berguiga@holydis.com>
2018-02-06 15:27:42 +0100

    Fix: modifier la configuration de projet.

---
+++ src/main/java/tn/bergit/realtime/RealTimeAppApplication.java
@@ -0,0 +1,17 @@
+package tn.bergit.realtime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+//@EntityScan( basePackages = {"tn.bergit.relatime.bo"} )
+//@ComponentScan("tn.bergit.relatime")
+//@EnableJpaRepositories("tn.bergit.relatime.repository")
+public class RealTimeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealTimeAppApplication.class, args);
+	}
+}
